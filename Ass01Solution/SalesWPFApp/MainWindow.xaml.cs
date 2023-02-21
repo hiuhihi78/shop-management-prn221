08677e2d151fd118f5d082aeb05112b4b5ff7e32
hiuhihi78 <leduchieu2001x@gmail.com>
2023-02-21 18:15:20 +0700

    add source Member Management screen

--- Ass01Solution/SalesWPFApp/MainWindow.xaml.cs
+++ Ass01Solution/SalesWPFApp/MainWindow.xaml.cs
@@ -27,5 +27,23 @@ namespace SalesWPFApp
             NavigationService.Initialize(MainFrame);
             NavigationService.NavigateTo(new Login());
         }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
     }
 }
---
+++ Ass01Solution/SalesWPFApp/ViewModels/HomeViewModel.cs
@@ -0,0 +1,45 @@
+﻿using SalesWPFApp.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using SalesWPFApp.Views;
+
+namespace SalesWPFApp.ViewModels
+{
+    public class HomeViewModel : INotifyPropertyChanged
+    {
+        #region Declare varibles
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+        public HomeViewModel()
+        {
+
+        }
+
+        #region management member
+        private RelayCommand memberManagementCommand;
+
+        public RelayCommand MemberManagementCommand
+        {
+            get { return memberManagementCommand; }
+            set { memberManagementCommand = value; }
+        }
+
+        private void HandleMemberManagement()
+        {
+            Navigation.NavigationService.NavigateTo(new MemeberManagement());
+        }
+
+        #endregion
+    }
+}
